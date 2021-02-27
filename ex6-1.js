c3911db10989e82e3cf0dcf64c123371eb0194ec
NanamiNegishi <zm18104@josai.ac.jp>
2021-02-27 10:55:58 +0000

    0227

--- ex6-1.js
+++ ex6-1.js
@@ -3,3 +3,6 @@ var who61 = 'world';
 function sayhello61() {alert('Hello, ' + who61 + '!');}
 function taro61() {who61 = '太郎';}
 function hanako61() {who61 = '花子';}
+function saygoodbye61() {alert('goodbye, Function!');}
+function saygoodbye61() {alert('goodbye, ' + who61 + '!');}
+
